99d3b436255c665223a2061236390d923fd64f20
Exzotic5485 <61587328+Exzotic5485@users.noreply.github.com>
2025-01-27 14:00:53 +0000

    update cover image source

--- src/components/card-set.tsx
+++ src/components/card-set.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent } from "@/components/ui/card";
+import { IMAGE_SOURCE_URL } from "@/lib/constants";
 import type { CardSet } from "@/lib/db";
 import { Link } from "@tanstack/react-router";
 
@@ -14,7 +15,7 @@ export function CardSet({ set }: CardSetProps) {
         >
             <Card className="h-full">
                 <img
-                    src={set.image}
+                    src={IMAGE_SOURCE_URL + set.image}
                     className="rounded-t-[inherit] w-full h-auto"
                 />
                 <CardContent className="py-4">
