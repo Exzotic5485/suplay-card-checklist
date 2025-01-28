260665ec014d2d25035e5dc1e7780ec83b358abb
Exzotic5485 <61587328+Exzotic5485@users.noreply.github.com>
2025-01-28 11:05:47 +0000

    parallel image uploads

--- scripts/upload-images.ts
+++ scripts/upload-images.ts
@@ -3,43 +3,53 @@ import { s3 } from "bun";
 const SUPLAY_URL = "https://img.suplaymart.com/img";
 
 async function main() {
-    const [, , inputFilePath, ...setIds] = process.argv;
+    const [, , inputFilePath] = process.argv;
 
-    if (process.argv.length < 4) {
-        throw new Error("Must provide args: `<input file> <...set ids>`");
+    if (process.argv.length < 3) {
+        throw new Error("Must provide args: `<input file>`");
     }
 
     const inputFile = Bun.file(inputFilePath);
 
-    const checklist = await inputFile.json();
+    const set = await inputFile.json();
 
     const failedIds: string[] = [];
 
-    for (const setId of setIds) {
-        const set = checklist[setId];
+    const taskQueue = new TaskQueue(async (cardId: string, i) => {
+        const response = await fetch(`${SUPLAY_URL}/${cardId}.png`);
 
-        if (!set) {
-            console.log(
-                `'${setId}' is not present in the checklist. Skipping...`
-            );
-            continue;
+        if (response.status !== 200) {
+            console.log(`Failed to fetch card id '${cardId}'`);
+            failedIds.push(cardId);
+            return;
         }
 
-        for (const card of set.cards) {
-            const response = await fetch(`${SUPLAY_URL}/${card.id}.png`);
+        await s3.write(`${cardId}.png`, response);
 
-            if (response.status !== 200) {
-                console.log(`Failed to fetch card id '${card.id}'`);
-                failedIds.push(card.id);
-                continue;
-            }
+        console.log(
+            `Uploaded ${cardId}.png to s3. (${i} / ${set.cards.length})`
+        );
+    }, 8);
 
-            await s3.write(`${card.id}.png`, response);
-            console.log(`Uploaded '${card.id}' to s3.`);
-        }
-    }
+    await taskQueue.run(set.cards.map((card, i) => [card.id, i + 1]));
 
     await Bun.write("./failures.json", JSON.stringify(failedIds, null, 4));
+}
+
+class TaskQueue<T> {
+    constructor(
+        public taskFn: (...args: T[]) => void,
+        public parallel = 4
+    ) {}
+
+    async run(argsArr: T[][]) {
+        for (let i = 0; i < argsArr.length; i += this.parallel) {
+            const chunk = argsArr.slice(i, i + this.parallel);
+            const chunkPromises = chunk.map((args) => this.taskFn(...args));
+
+            await Promise.all(chunkPromises);
+        }
+    }
 }
 
 await main();
